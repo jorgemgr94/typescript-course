b53c695a47dd607058d368ee68190896c66a49d4
Jorge Garcia <jorgemgr94@gmail.com>
2022-01-29 13:58:17 -0600

    Singleton pattern added

---
+++ src/designPatterns/creational/singleton.ts
@@ -0,0 +1,22 @@
+/**
+ * Ensure a class only has one instance, and provide a global point of access to it.
+ */
+class Hairdresser {
+  private static instance: Hairdresser | undefined = undefined;
+  private constructor() {}
+
+  static getInstance(): Hairdresser {
+    if (!Hairdresser.instance) {
+      Hairdresser.instance = new Hairdresser();
+    }
+
+    return Hairdresser.instance;
+  }
+  // more stuff...
+}
+
+// Constructor of class 'Hairdresser' is private and only accessible within the class declaration.
+// const hairdresser: Hairdresser = new Hairdresser();
+
+const hairdresser: Hairdresser = Hairdresser.getInstance();
+console.log(hairdresser);
