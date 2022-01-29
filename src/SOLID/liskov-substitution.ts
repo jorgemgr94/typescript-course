594d532c001e3594fad2f44457d1017921df0520
Jorge Garcia <jorgemgr94@gmail.com>
2022-01-29 14:57:52 -0600

    SOLID liskov substitution added

--- src/SOLID/liskov-substitution.ts
+++ src/SOLID/liskov-substitution.ts
@@ -1,1 +1,35 @@
-// TODO
+/**
+ * Subtypes must be substitutable for their base types.
+ */
+interface IPersistanceService {
+  save(entity: any): number;
+}
+class LocalStoragePersistanceService implements IPersistanceService {
+  save(): number {
+    var id = Math.floor(Math.random() * 100 + 1);
+    // Local storage persistance logic...
+    return id;
+  }
+}
+
+class CookiePersistanceService implements IPersistanceService {
+  save(): number {
+    var id = Math.floor(Math.random() * 100 + 1);
+    // Cookie persistance logic...
+    return id;
+  }
+}
+
+class FavoritesController {
+  constructor(private persistanceService: IPersistanceService) {}
+
+  public saveAsFavorite(articleId: number) {
+    return this.persistanceService.save(articleId);
+  }
+}
+
+// FavoritesController is doesn't affected by IPersistanceService.
+const favController = new FavoritesController(
+  new LocalStoragePersistanceService()
+);
+// const favController = new FavoritesController(new CookiePersistanceService());
