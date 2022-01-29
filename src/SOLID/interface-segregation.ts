578e34ae9b14b906edd080d78db0a2caa02dad9e
Jorge Garcia <jorgemgr94@gmail.com>
2022-01-29 14:28:05 -0600

    SOLID interface segregation added

--- src/SOLID/interface-segregation.ts
+++ src/SOLID/interface-segregation.ts
@@ -1,1 +1,24 @@
-// TODO
+/**
+ * The interface segregation principle states that no client should be forced to
+ * depend on methods it does not use. By putting too many properties in our interfaces,
+ * we risk breaking the rule.
+ */
+interface CanEdit {
+  edit(): void;
+}
+interface CanView {
+  view(): void;
+}
+class AdminRole implements CanEdit, CanView {
+  public edit() {
+    /// ...
+  }
+  public view() {
+    /// ...
+  }
+}
+class BasicRole implements CanView {
+  public view() {
+    /// ...
+  }
+}
