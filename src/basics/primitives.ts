d0d2d0abb38a26d2db50c142d49fb0403c8fab65
Jorge García <116111429+jorgegzen@users.noreply.github.com>
2024-10-12 01:06:26 -0600

    feat(basics): tweak primitives demo

--- src/basics/primitives.ts
+++ src/basics/primitives.ts
@@ -1,12 +1,44 @@
-type Primitives = {
-  number: number;
-  string: string;
-  boolean: boolean;
-  null: null;
-  undefined: undefined;
-  symbol: symbol;
+type Primitive = string | number | bigint | boolean | symbol |
+  null | undefined;
+
+export interface Product {
+  id: symbol; // Unique identifier
+  name: string;
+  price: number;
+  available: boolean;
+  totalSales: bigint; // Large number representing total sales
 }
 
+// The filter definition
+interface FilterDefinition {
+  key: keyof Product;
+  value: Primitive;
+}
+
+// Function to filter products
+function filterProducts(
+  products: Product[],
+  filters: FilterDefinition[]
+): Product[] {
+  return products.filter((product) => {
+    return filters.every((filter) => {
+      return product[filter.key] === filter.value;
+    });
+  });
+}
+
+filterProducts([
+  {
+    id: Symbol("1"), name: "Laptop",
+    price: 1245, available: true,
+    totalSales: BigInt(100),
+  },
+], [
+  { key: "available", value: true },
+  { key: "price", value: 450 },
+])
+
+// ================================================
 
 type ObjectTypes = {
   object: object;
